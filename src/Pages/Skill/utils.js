0718469dd426959d9d969e3eb7da3d637ca4c252
Shubham8564 <121994568+Shubham8564@users.noreply.github.com>
2023-10-27 18:47:08 +0530

    Add files via upload

---
+++ src/Pages/Skill/utils.js
@@ -0,0 +1,78 @@
+export const skillData = [
+  {
+    labels: "Front End",
+    data: [
+      {
+        skillName: "HTML",
+        percetage: "90",
+      },
+      {
+        skillName: "CSS",
+        percetage: "85",
+      },
+      {
+        skillName: "JavaScript",
+        percetage: "70",
+      },
+      {
+        skillName: "React Js",
+        percetage: "75",
+      },
+    ],
+  },
+  {
+    labels: "Back End",
+    data: [
+      {
+        skillName: "Java",
+        percetage: "70",
+      },
+
+      {
+        skillName: "Hibernate",
+        percetage: "80",
+      },
+      {
+        skillName: "Spring",
+        percetage: "60",
+      },
+      {
+        skillName: "Spring Boot",
+        percetage: "70",
+      },
+    ],
+  },
+  {
+    labels: "Database",
+    data: [
+      {
+        skillName: "SQL",
+        percetage: "85",
+      }
+      
+    ],
+  },{
+  labels: "Tools/Platform",
+    data: [
+      {
+        skillName: "GIT ( Version Control System )",
+        percetage: "70",
+      },
+      {
+        skillName: "GitHub",
+        percetage: "70",
+      },
+      {
+        skillName: "Eclipse",
+        percetage: "70",
+      },
+      {
+        skillName: "VS Code",
+        percetage: "70",
+      },
+      {
+        skillName: "STS ( Spring Tool Suite )",
+        percetage: "70",
+      },
+    ]}
+];
